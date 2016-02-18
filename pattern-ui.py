8c55121da5a56bde876ef751a5551abc8af3b44d
Michaela Barrett <git@rose-labyrinth.com>
2016-02-18 09:06:15 -0800

    Updated pattern-ui to accept quantity and indent arguments

--- pattern-ui.py
+++ pattern-ui.py
@@ -2,14 +2,17 @@
 Testing UI for pattern.py. Demonstrates one way to:
 1) print pattern tree with formatting
 2) access pattern functioncs dynamically
+
+Reminder: When you call pattern_ui, the pattern_name argument
+must be submitted in quotes ('quartz' NOT quartz).
 """
 
 import pattern
 
 TAB_STR = "\t"
 
-def pattern_ui(pattern_name):
-    getattr(pattern,pattern_name)()
+def pattern_ui(pattern_name,quan=1,indent=0):
+    getattr(pattern,pattern_name)(quan,indent)
     for x in pattern.pattern_tree:
         print TAB_STR*x['indent'] + "[" + x['name'] + "] x%d" % (x['quan'])
     pattern.pattern_tree = []
